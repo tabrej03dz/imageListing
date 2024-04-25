35e31d59d766e50b1c618023061eacd278576cad
tabrej03dz <vijay@redhorntech.com>
2024-04-25 17:17:05 +0530

    customer index

--- resources/views/backend/customer/index.blade.php
+++ resources/views/backend/customer/index.blade.php
@@ -1,7 +1,7 @@
 @extends('backend.layout.root', ['title' => 'Customers'])
 @section('content')
 
-    @if($customers->count() > 0)
+
         <!-- DataTales Example -->
         <div class="card shadow mb-4">
             <div class="card-header py-3 d-flex justify-content-between align-items-center">
@@ -11,6 +11,7 @@
                     <a href="{{ route('customer.upload') }}" class="btn btn-primary">Customer Upload</a>
                 </div>
             </div>
+            @if($customers->count() > 0)
             <div class="card-body">
                 <div class="table-responsive">
                     <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
@@ -46,10 +47,10 @@
                     </table>
                 </div>
             </div>
+                {{$customers->links()}}
+            @else
+                <p>No customers found.</p>
+            @endif
         </div>
-{{$customers->links()}}
-    @else
-        <p>No customers found.</p>
-    @endif
 
 @endsection
