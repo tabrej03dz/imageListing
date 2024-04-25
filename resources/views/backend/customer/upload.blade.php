d8805676071d82900cdea08bd1c11f19c72cf26f
tabrej03dz <vijay@redhorntech.com>
2024-04-25 18:22:38 +0530

    some ui changes in header

--- resources/views/backend/customer/upload.blade.php
+++ resources/views/backend/customer/upload.blade.php
@@ -1,8 +1,9 @@
 @extends('backend.layout.root', ['title' => 'Create Customer'])
+
 @section('content')
 
     @if($errors->any())
-        <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded mb-4">
+        <div class="alert alert-danger">
             <ul>
                 @foreach($errors->all() as $error)
                     <li>{{ $error }}</li>
@@ -12,28 +13,15 @@
     @endif
 
     <h2 class="text-2xl font-semibold text-gray-800 mb-4">Create Customer</h2>
-    @if($errors->any())
-        <div>
-            <ul>
-                @foreach($errors->all() as $error)
-                    <li>{{$error}}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
-    <form action="{{route('customer.import')}}" method="POST" enctype="multipart/form-data">
+
+    <form action="{{ route('customer.import') }}" method="POST" enctype="multipart/form-data">
         @csrf
         <div class="mb-4">
-            <label for="title" class="block text-gray-700 text-sm font-bold mb-2">Upload a excel file:</label>
-            <input type="file" id="file" name="file"  class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500">
+            <label for="file" class="form-label">Upload an Excel file:</label>
+            <input type="file" id="file" name="file" class="form-control">
         </div>
 
-
-
-        <div class="flex">
-            <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Submit</button>
-        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
     </form>
-
 
 @endsection
