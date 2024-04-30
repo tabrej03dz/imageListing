efcc910f5e4c7b3406670cab87b9c3dfa20929cf
tabrej03dz <tabrejalam770496@gmail.com>
2024-04-30 15:14:21 +0530

    layout code optimize

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,38 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>Search Images</title>
-
-    <!-- Custom fonts for this template-->
-    <link href="{{asset('assets/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
-
-    <!-- Custom styles for this template-->
-    <link href="{{asset('assets/css/sb-admin-2.min.css')}}" rel="stylesheet">
-
-    <link rel="icon" type="image/png" href="{{asset('assets/favicon.jpg')}}">
-
-</head>
-
-<body class="bg-gradient-primary">
-
-<div class="container">
-
+@extends('frontLayout', ['title' => 'Search Image'])
+@section('content')
     <!-- Outer Row -->
     <div class="row justify-content-center">
-
         <div class="col-xl-6 col-lg-8 col-md-10">
-
             <div class="card o-hidden border-0 shadow-lg my-5">
                 <div class="card-body p-0">
                     <!-- Nested Row within Card Body -->
@@ -50,9 +20,9 @@
                                 </div>
                             @endif
                             @if(session('error'))
-                                    <div class="alert alert-danger">
-                                            <li>{{session('error')}}</li>
-                                    </div>
+                                <div class="alert alert-danger">
+                                    <li>{{session('error')}}</li>
+                                </div>
                             @endif
                             <div class="p-5">
                                 <div class="text-center">
@@ -70,34 +40,18 @@
                                     </button>
                                 </form>
                                 <hr>
-{{--                                <div class="text-center">--}}
-{{--                                    <a class="small" href="forgot-password.html">Forgot Password?</a>--}}
-{{--                                </div>--}}
-{{--                                <div class="text-center">--}}
-{{--                                    <a class="small" href="{{route('register')}}">Register!</a>--}}
-{{--                                </div>--}}
+                                {{--                                <div class="text-center">--}}
+                                {{--                                    <a class="small" href="forgot-password.html">Forgot Password?</a>--}}
+                                {{--                                </div>--}}
+                                {{--                                <div class="text-center">--}}
+                                {{--                                    <a class="small" href="{{route('register')}}">Register!</a>--}}
+                                {{--                                </div>--}}
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-
         </div>
-
     </div>
+@endsection
 
-</div>
-
-<!-- Bootstrap core JavaScript-->
-<script src="{{asset('assets/vendor/jquery/jquery.min.js')}}"></script>
-<script src="{{asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-
-<!-- Core plugin JavaScript-->
-<script src="{{asset('assets/vendor/jquery-easing/jquery.easing.min.js')}}"></script>
-
-<!-- Custom scripts for all pages-->
-<script src="{{asset('assets/js/sb-admin-2.min.js')}}"></script>
-
-</body>
-
-</html>
