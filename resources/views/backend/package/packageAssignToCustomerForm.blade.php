8a872c98caec7908241faab5eb390492af8b78cf
tabrej03dz <tabrejalam770496@gmail.com>
2024-06-10 15:45:03 +0530

    some changes

--- resources/views/backend/package/packageAssignToCustomerForm.blade.php
+++ resources/views/backend/package/packageAssignToCustomerForm.blade.php
@@ -13,13 +13,13 @@
 
         <div class="row justify-content-around">
             <div class="col-md-8">
-                <h2 class="text-2xl font-semi-bold text-gray-800 mb-4">Create Package</h2>
+                <h2 class="text-2xl font-semi-bold text-gray-800 mb-4">Assign Package to customer</h2>
                 <form action="{{ route('package.assignToCustomer', ['package' => $package]) }}" method="POST">
                     @csrf
                     <div class="mb-3">
-                        <label for="customer_id" class="form-label">Select a package:</label>
+                        <label for="customer_id" class="form-label">Select a customer:</label>
                         <select name="customer_id" id="" class="form-control">
-                            <option disabled value="">Select a package</option>
+                            <option disabled value="">Select a Customer</option>
                             @foreach($customers as $customer)
                                 <option value="{{$customer->id}}">{{$customer->name}}</option>
                             @endforeach
