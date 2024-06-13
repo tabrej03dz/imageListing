64aad7b5a553fadcce818463c9af35bf22f5c9b3
tabrej03dz <tabrejalam770496@gmail.com>
2024-06-13 15:55:05 +0530

    migration files to add deleted at column in multiple table

--- database/migrations/2024_06_13_155001_add_deleted_at_column_in_notes_table.php
+++ database/migrations/2024_06_13_155001_add_deleted_at_column_in_notes_table.php
@@ -11,12 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('notes', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->text('description');
-            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
-            $table->timestamps();
+        Schema::table('notes', function (Blueprint $table) {
+            $table->softDeletes();
         });
     }
 
@@ -25,6 +21,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('notes');
+        Schema::table('notes', function (Blueprint $table) {
+            $table->dropSoftDeletes();
+        });
     }
 };
