1d15c58af85c99a58db12be4f56d2ba8d9d8ee62
tabrej03dz <tabrejalam770496@gmail.com>
2024-06-25 15:44:55 +0530

    send images using ajax request

--- resources/views/backend/image/index.blade.php
+++ resources/views/backend/image/index.blade.php
@@ -72,7 +72,7 @@
                                         {{--    success: function(response) {--}}
                                         //         count = response.count;
                                                 var j = 1;
-                                                for (var i = 1; i<=5; i++){
+                                                for (var i = 1; i<=100; i++){
                                                     $.ajax({
                                                         url: '{{ route("images.sendImage", ['date' => $date]) }}',
                                                         method: 'GET',
