428d829ad1a03457081bcd88eeb3b19b71746542
tabrej03dz <tabrejalam770496@gmail.com>
2024-08-21 15:04:44 +0530

    countries and states migration file

---
+++ database/migrations/2024_08_21_144808_create_countries_table.php
@@ -0,0 +1,59 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('countries', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100)->charset('utf8mb4')->collation('utf8mb4_unicode_ci');
+            $table->char('iso3', 3)->nullable()->charset('utf8mb4')->collation('utf8mb4_unicode_ci');
+            $table->char('numeric_code', 3)->nullable()->charset('utf8mb4')->collation('utf8mb4_unicode_ci');
+            $table->char('iso2', 2)->nullable()->charset('utf8mb4')->collation('utf8mb4_unicode_ci');
+            $table->string('phonecode')->nullable()->charset('utf8mb4')->collation('utf8mb4_unicode_ci');
+            $table->string('capital')->nullable()->charset('utf8mb4')->collation('utf8mb4_unicode_ci');
+            $table->string('currency')->nullable()->charset('utf8mb4')->collation('utf8mb4_unicode_ci');
+            $table->string('currency_name')->nullable()->charset('utf8mb4')->collation('utf8mb4_unicode_ci');
+            $table->string('currency_symbol')->nullable()->charset('utf8mb4')->collation('utf8mb4_unicode_ci');
+            $table->string('tld')->nullable()->charset('utf8mb4')->collation('utf8mb4_unicode_ci');
+            $table->string('native')->nullable()->charset('utf8mb4')->collation('utf8mb4_unicode_ci');
+            $table->string('region')->nullable()->charset('utf8mb4')->collation('utf8mb4_unicode_ci');
+            $table->unsignedMediumInteger('region_id')->nullable();
+            $table->string('subregion')->nullable()->charset('utf8mb4')->collation('utf8mb4_unicode_ci');
+            $table->unsignedMediumInteger('subregion_id')->nullable();
+            $table->string('nationality')->nullable()->charset('utf8mb4')->collation('utf8mb4_unicode_ci');
+            $table->text('timezones')->nullable()->charset('utf8mb4')->collation('utf8mb4_unicode_ci');
+            $table->text('translations')->nullable()->charset('utf8mb4')->collation('utf8mb4_unicode_ci');
+            $table->decimal('latitude', 10, 8)->nullable();
+            $table->decimal('longitude', 11, 8)->nullable();
+            $table->string('emoji', 191)->nullable()->charset('utf8mb4')->collation('utf8mb4_unicode_ci');
+            $table->string('emojiU', 191)->nullable()->charset('utf8mb4')->collation('utf8mb4_unicode_ci');
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+            $table->tinyInteger('flag')->default(1);
+            $table->string('wikiDataId')->nullable()->charset('utf8mb4')->collation('utf8mb4_unicode_ci')->comment('Rapid API GeoDB Cities');
+
+            $table->foreign('region_id')->references('id')->on('regions');
+            $table->foreign('subregion_id')->references('id')->on('subregions');
+
+            $table->index('region_id', 'country_continent');
+            $table->index('subregion_id', 'country_subregion');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('countries');
+    }
+};
---
+++ database/migrations/2024_08_21_150313_create_states_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('states', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 255)->charset('utf8mb4')->collation('utf8mb4_unicode_ci');
+            $table->mediumInteger('country_id')->unsigned();
+            $table->char('country_code', 2)->charset('utf8mb4')->collation('utf8mb4_unicode_ci');
+            $table->string('fips_code', 255)->charset('utf8mb4')->collation('utf8mb4_unicode_ci')->nullable();
+            $table->string('iso2', 255)->charset('utf8mb4')->collation('utf8mb4_unicode_ci')->nullable();
+            $table->string('type', 191)->charset('utf8mb4')->collation('utf8mb4_unicode_ci')->nullable();
+            $table->decimal('latitude', 10, 8)->nullable();
+            $table->decimal('longitude', 11, 8)->nullable();
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+            $table->boolean('flag')->default(1);
+            $table->string('wikiDataId', 255)->charset('utf8mb4')->collation('utf8mb4_unicode_ci')->nullable()->comment('Rapid API GeoDB Cities');
+
+            $table->primary('id');
+            $table->index('country_id', 'country_region');
+            $table->foreign('country_id', 'country_region_final')
+                ->references('id')
+                ->on('countries')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('states');
+    }
+};
